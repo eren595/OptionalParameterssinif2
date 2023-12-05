f2082f09676592397138100755b5ac28c2c588d5
eren595 <eren595@BPC06>
2023-12-05 09:20:00 +0300

    Proje dosyası ekle.

---
+++ OptionalParameterssinif2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterssinif2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinifim sinifim = new Sinifim();
+            sinifim.Selamla();
+            sinifim.Selamla(" Defne");
+        }
+    }
+    class Sinifim
+    {
+        public void Selamla(string ad = " Emre")
+        {
+            Console.WriteLine("Merhaba" +ad);
+        }
+    }
+}
